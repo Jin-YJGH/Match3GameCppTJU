e63ae6919f7c0d649c40188d90476610147b9400
Kyq-ereshkigal <62504432+Kyq-ereshkigal@users.noreply.github.com>
2020-06-03 15:28:09 +0800

    Add files via upload
    volume change and open

--- Classes/SettingScene.cpp
+++ Classes/SettingScene.cpp
@@ -1,1 +1,55 @@
 #include "SettingScene.h"
+#include "HelloWorldScene.h"
+#include "ui/CocosGUI.h" 
+#include"GUI/CCControlExtension/CCInvocation.h"
+
+using namespace ui;
+using namespace CocosDenshion;
+
+auto volume = Slider::create();
+volume->loadBarTexture("sliderback.png");//音量进度条背景
+volume->loadSlidBallTextures("sliderbutton.png", "sliderbutton.png", "");//滑动块（正常，按下，禁用
+volume->loadProgressBarTexture("silderback.png");//滑动条进度图片，即从左至滑动按钮显示的内容
+volume->setPercent(soundValue * 100);
+volume->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2));
+volume->addEventListener([=](Ref* pSender, Slider::EventType type) 
+	{
+	if (type == Slider::EventType::ON_PERCENTAGE_CHANGED) 
+	{
+		soundValue = volume->getPercent() * 0.01;
+		log("NOW=%.2f", soundValue);
+		// 设置背景音乐值
+		SimpleAudioEngine::getInstance()->setBackgroundMusicVolume(soundValue);
+		// 设置音效值
+		//SimpleAudioEngine::getInstance()->setEffectsVolume(soundValue);
+		UserDefault::getInstance()->setFloatForKey("soundNum", soundValue);
+		UserDefault::getInstance()->flush();
+	}
+	});
+this->addChild(volume);
+//声音开关
+auto soundControl = ControlSwitch::create
+(
+	Sprite::create("switch-mask.png"),
+	Sprite::create("switch-on.png"),
+	Sprite::create("switch-off.png"),
+	Sprite::create("switch-thumb.png"),
+	Label::createWithSystemFont("On", "Arial-BoldMT", 16),
+	Label::createWithSystemFont("Off", "Arial-BoldMT", 16)
+);
+soundControl->addTargetWithActionForControlEvents(this, cccontrol_selector(HelloWorld::soundSwitch), Control::EventType::VALUE_CHANGED);
+soundControl->setPosition(Vec2(visibleSize.width / 2, visibleSize.height * 0.3));
+this->addChild(soundControl);
+}
+
+void HelloWorld::soundSwitch(Ref* psender, Control::EventType controlevent) {
+	auto fSwitch = (ControlSwitch*)psender;
+	if (fSwitch->isOn()) {
+		//SimpleAudioEngine::getInstance()->resumeAllEffects();
+		SimpleAudioEngine::getInstance()->rewindBackgroundMusic();
+	}
+	else {
+		//SimpleAudioEngine::getInstance()->stopAllEffects();
+		SimpleAudioEngine::getInstance()->stopBackgroundMusic();
+	}
+}
--- Classes/SettingScene.h
+++ Classes/SettingScene.h
@@ -1,9 +1,11 @@
-#pragma once
 #ifndef __SETTING_SCENE_H__
 #define __SETTING_SCENE_H__
 
-#include "cocos2d.h"
+#include"GUI\CCControlExtension\CCControl.h"
+#include"GUI\CCControlExtension\CCControlSwitch.h"
+using namespace cocos2d::extension;
 
-USING_NS_CC;
+float soundValue;
+void soundSwitch(Ref* psender, Control::EventType controlevent);
 
 #endif // !__SETTING_SCENE_H__
