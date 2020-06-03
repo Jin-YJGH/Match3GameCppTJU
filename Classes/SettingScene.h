4e976480daf07262fb68e409244d0b07052e53bd
Kyq-ereshkigal <62504432+Kyq-ereshkigal@users.noreply.github.com>
2020-06-03 15:38:58 +0800

    Add files via upload

--- Classes/SettingScene.h
+++ Classes/SettingScene.h
@@ -1,10 +1,13 @@
+#pragma once
 #ifndef __SETTING_SCENE_H__
 #define __SETTING_SCENE_H__
 
+#include "cocos2d.h"
 #include"GUI\CCControlExtension\CCControl.h"
 #include"GUI\CCControlExtension\CCControlSwitch.h"
 using namespace cocos2d::extension;
 
+USING_NS_CC;
 float soundValue;
 void soundSwitch(Ref* psender, Control::EventType controlevent);
 
